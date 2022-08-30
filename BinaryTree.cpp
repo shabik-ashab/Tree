a3d8375ea50b374440fe01c45262ba52de805c5b
nub69 <shabik.ashab@gmail.com>
2022-08-30 12:34:16 +0600

    class for treeNode added

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class treeNode{
+public:
+    int data;
+    treeNode* leftChild;
+    treeNode* rightChild;
+
+    treeNode(int val){
+        data = val;
+        leftChild = NULL;
+        rightChild = NULL;
+    }
+};
+
 int main()
 {
     
