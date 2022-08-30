f97a169868c4419b640e849691d6fe5c9827de69
nub69 <shabik.ashab@gmail.com>
2022-08-30 12:41:00 +0600

    take tree input

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -17,5 +17,28 @@ public:
 int main()
 {
     
+    int n;
+    cin>>n;
+
+    treeNode* allNodes[n];
+
+    for(int i=0;i<n;i++){
+        allNodes[i]->data = -1;
+    }    
+
+    for(int i=0;i<n;i++){
+        int val,left,right;
+        cin>>val>>left>>right;
+
+        allNodes[i]->data = val;
+
+        if(left!=-1){
+            allNodes[i]->leftChild = allNodes[left];
+        }
+
+        if(right!=-1){
+            allNodes[i]->rightChild = allNodes[right];
+        }
+    }  
     return 0;
 }
